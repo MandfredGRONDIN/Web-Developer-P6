7937c63a1cb40f313147ed255b86d74ec260fc41
Mandfred GRONDIN <mandfred974@hotmail.fr>
2022-10-12 21:25:53 +0200

    Update
    Update .env pour url mongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_MAIL}:${process.env.DB_PASSWORD}@cluster0.0jmu1vj.mongodb.net/test`,
+mongoose.connect(`mongodb+srv://${process.env.DB}`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connection to MongoDB successful !"))
